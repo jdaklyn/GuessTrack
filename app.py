930d492f4501e31613dcdee5c6c2282b902ada09
jda <cyakalyon@gmail.com>
2026-08-26 23:02:55 +0300

    fix: bind host to 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def handle_disconnect():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
