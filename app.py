d45161094f55ec14ba9af6580351a6b41eb69ca4
jda <cyakalyon@gmail.com>
2026-08-26 23:58:48 +0300

    fix: set host to 0.0.0.0 and dynamic port for render deployment

--- app.py
+++ app.py
@@ -297,4 +297,6 @@ def on_disconnect():
                 break
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
